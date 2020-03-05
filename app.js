dd96cf378e7d45bb07305fdb31de9b310a104267
MelvinManni <melvinnnamani@gmail.com>
2020-03-05 03:43:15 +0100

    Initial Commit

---
+++ app.js
@@ -0,0 +1,57 @@
+let openBtn = document.getElementById("open");
+let closeBtn = document.getElementById("close");
+
+openBtn.addEventListener("click", ()=> {
+    document.getElementById("menu-group").classList.add = "slide-in"
+    document.getElementById("menu-group").style.display = "block";
+});
+
+
+closeBtn.addEventListener("click", ()=>{
+
+    document.getElementById("menu-group").classList.add = "slide-out";
+    document.getElementById("menu-group").style.display = "none";
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+hoverEffect =()=>{
+    if (MouseEvent){
+
+        document.getElementById("arrow").style.backgroundColor = "transparent";
+        document.getElementById("arrow").style.borderColor = "rgb(129, 94, 50)";
+
+        document.getElementById("readBtn").style.color = "rgb(248, 243, 237)";
+        document.getElementById("readBtn").style.backgroundColor = "rgb(129, 94, 50)";
+
+        document.getElementById("arrowIcon").style.color = "rgb(129, 94, 50)";
+    }
+    
+
+}
+
+endHover =(e)=>{
+
+
+    document.getElementById("arrow").style.backgroundColor = "rgb(129, 94, 50)";
+    document.getElementById("arrow").style.borderColor = "rgb(129, 94, 50)";
+
+    document.getElementById("readBtn").style.color = "rgb(0, 0, 0)";
+    document.getElementById("readBtn").style.backgroundColor = "transparent";
+
+    document.getElementById("arrowIcon").style.color = "rgb(248, 243, 237)";
+
+}
